296b44c26f98aa0d7fbbc3bd4a6bc65395f3fedc
jiyoung yoon <65146746+hoiho77@users.noreply.github.com>
2021-01-05 01:25:55 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,8 @@
 def load_model(model_name):
+  if model_name == 'alexnet' :
+    from models.alexnet import Alexnet
+    model = Alexnet()
+    return model
+  
+  else : 
+    print("The model_name is not exists.")
