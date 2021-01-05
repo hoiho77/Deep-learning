f53c0e8d0a37eb0e2bcdd65ebfa143edb0635d6e
jiyoung yoon <65146746+hoiho77@users.noreply.github.com>
2021-01-05 11:47:59 +0900

    Update train.py

--- train.py
+++ train.py
@@ -12,32 +12,30 @@ parser.add_argument('--Epochs',type=int, default=10)
 parser.add_argument('--BATCH_SIZE', type=int, default=32)
 parser.add_argument('--LR', type=float, default=0.001)
 
-def process_images(image, label):
-    image = tf.image.per_image_standardization(image)
-    image = tf.image.resize(image, (227,227))
-    return image, label
-
-if__name__ == '__main__' :
+if __name__ == '__main__' :
     args = parser.parse_args()
 
-    model = args.model
+    model_name = args.model
     learning_rate = args.LR
     epochs = args.Epochs
     batch_size = args.BATCH_SIZE
     
-    train_ds, test_ds, validation_ds = utils.load_datasets(batch_size)
+    train_ds, test_ds, validation_ds = load_datasets(batch_size)
     
-    model = utils.load_model()
+    model = load_model(model_name)
     model.compile(loss='sparse_categorical_crossentropy', optimizer=tf.optimizers.SGD(lr=learning_rate), metrics=['accuracy'])
     model.summary()
     
+    run_logdir = get_run_logdir()
+    tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+
     history = model.fit(train_ds,
-          EPOCHS=epochs,
+          epochs=epochs,
           validation_data=validation_ds,
           validation_freq=1,
           callbacks=[tensorboard_cb])
     
     model.save('./models')
-    utils.graph(history)
+    graph(history)
    
     print("Accuracy: %2.2f" % (model.evaluate(test_ds)[1]))
